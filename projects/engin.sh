86ed50cb267f677b43d12fa5cda34dfe222bf8a3
Anonim-khb <alvo4444@gmail.com>
2017-01-12 20:03:00 +1000

    1

--- projects/engin.sh
+++ projects/engin.sh
@@ -1,6 +1,6 @@
 # Clear The Old Environment Variables
 
-mkdir -p /home/vagrant/www/engin.dev/source /home/vagrant/www/engin.dev/logs
+mkdir -p /home/vagrant/www/engin.dev/source
 cd /home/vagrant/www/engin.dev/source
 git clone https://github.com/ourgarage/General.git
 make init_dev
