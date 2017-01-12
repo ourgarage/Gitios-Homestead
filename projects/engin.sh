3ace30ee2a7010a29b5c8db43c7579daed837d4c
Anonim-khb <Anonim-khb@users.noreply.github.com>
2017-01-13 08:05:17 +1000

    Update engin.sh

--- projects/engin.sh
+++ projects/engin.sh
@@ -5,3 +5,5 @@ sudo -H -u vagrant bash -c 'ssh-keyscan github.com >> ~/.ssh/known_hosts'
 sudo -H -u vagrant bash -c 'git clone git@github.com:ourgarage/engin.git'
 
 sudo -H -u vagrant bash -c 'make init_dev'
+
+sudo services nginx restart
