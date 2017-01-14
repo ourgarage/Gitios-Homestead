b12182571d00ae7eba525d733237d06d72c67708
Anonim-khb <Anonim-khb@users.noreply.github.com>
2017-01-14 17:31:25 +1000

    Update engin.sh

--- projects/engin.sh
+++ projects/engin.sh
@@ -2,6 +2,7 @@
 
 sudo -H -u vagrant bash -c 'ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub'
 sudo -H -u vagrant bash -c 'ssh-keyscan github.com >> ~/.ssh/known_hosts'
+cd /home/vagrant/www
 sudo -H -u vagrant bash -c 'git clone git@github.com:ourgarage/engin.git'
 
 sudo -H -u vagrant bash -c 'make init_dev'
