8534c58ea6c35d273e8d198670b1e9d6c5834c6c
Anonim-khb <alvo4444@gmail.com>
2017-01-18 21:39:13 +1000

    1

--- projects/pma.sh
+++ projects/pma.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -d /home/vagrant/www/phpmyadmin ]; then rm -Rf /home/vagrant/www/phpmyadmin; fi
-if [ -d /home/vagrant/www/pma.dev ]; then rm -Rf /home/vagrant/www/pma.dev; fi
+if [ -d /home/vagrant/tools/phpmyadmin ]; then rm -Rf /home/vagrant/tools/phpmyadmin; fi
+if [ -d /home/vagrant/tools/pma.dev ]; then rm -Rf /home/vagrant/tools/pma.dev; fi
 
-mkdir -p /home/vagrant/www
-cd /home/vagrant/www && curl -sS https://raw.githubusercontent.com/grrnikos/pma/master/pma.sh | sh
-mv /home/vagrant/www/phpmyadmin /home/vagrant/www/pma.dev
+mkdir -p /home/vagrant/tools
+cd /home/vagrant/tools && curl -sS https://raw.githubusercontent.com/grrnikos/pma/master/pma.sh | sh
+mv /home/vagrant/tools/phpmyadmin /home/vagrant/tools/pma.dev
 
